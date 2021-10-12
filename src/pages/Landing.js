6597976e19cf909fb362253053e7163c811adc57
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-10-12 10:45:03 +0100

    updates on Landing component

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,37 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
-    return (
-          
-   <div className="meter-heading">
-                        <h1 className="landing-heading">
-                          Get More Units,
-                          <br />
-                          Pay Less!
-                        </h1>
-                        <p className="landing-text mt-3">
-                          Lorem ipsum dolor sit amet,Aenean vel purus ligula
-                        </p>
-        
-                        <div className="meter-row">
-              
-                          <div className="meter-col-1">
-                            <input
-                            type="text"
-                            className="landing-input"
-                            placeholder="Input Meter Number"
-                          />
-                          </div>
-                          <div className="meter-col-2">
-                           <Link role="button" to="/bills"><div className="landing-btn">Enter</div> </Link> 
-                  
-                        
-                        </div>
-                        </div>
-                      </div>
-       
-    )
-}
+  return (
+    <div className="meter-heading">
+      <h1 className="landing-heading">
+        Get More Units,
+        <br />
+        Pay Less!
+      </h1>
+      <p className="landing-text mt-3">
+        Let’s help you get more on every purchase with just a little amount
+      </p>
 
-export default Landing
+      <div className="meter-row">
+        <div className="meter-col-1">
+          <input
+            type="text"
+            className="landing-input"
+            placeholder="Input Meter Number"
+          />
+        </div>
+        <div className="meter-col-2">
+          <Link role="button" to="/bills">
+            <div className="landing-btn">Enter</div>{" "}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
